ca078a7ff00697eab92da2ac566088d0c59e5b37
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-19 16:34:38 +0000

    Deploy tom-drake/reselect-workshop to github.com/tom-drake/reselect-workshop.git:gh-pages

---
+++ precache-manifest.a3f1da339f56e66e17d92c2fab352068.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4373643b07e61a4d0e",
+    "url": "/reselect-workshop/static/js/main.bd437364.chunk.js"
+  },
+  {
+    "revision": "244d825dca7982a7b811",
+    "url": "/reselect-workshop/static/js/1.244d825d.chunk.js"
+  },
+  {
+    "revision": "b28c3374b9122ca19d60",
+    "url": "/reselect-workshop/static/js/runtime~main.b28c3374.js"
+  },
+  {
+    "revision": "bebc3e4e812989c0a08f8f4ec0c5c204",
+    "url": "/reselect-workshop/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reselect-workshop/precache-manifest.a3f1da339f56e66e17d92c2fab352068.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reselect-workshop/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
